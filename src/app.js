189d4240f754ab098d0fe5e778d9bb478465f65b
pramod429-git <18kppramod@gmail.com>
2025-02-11 14:10:18 +0530

    qurey param and dynamic rounte

--- src/app.js
+++ src/app.js
@@ -10,15 +10,22 @@ const app = express();
 // });
 
 app.get("/user", (req, res) => {
+  console.log(req.query); //{ id: '123', password: '321' }
   res.send("Hi this from server");
 });
 
-app.post("/user", (req, res) => {
-  res.send("data is save successfully!");
-});
+// app.get("/user/:userId/:userPw", (req, res) => {
+//   //   console.log(req.params); //{ userId: '123', userPw: '321' }
+//   console.log(req.query); //{}
+//   res.send("Hi this from server");
+// });
 
-app.delete("/user", (req, res) => {
-  res.send("data is deleted successfully!");
-});
+// app.post("/user", (req, res) => {
+//   res.send("data is save successfully!");
+// });
+
+// app.delete("/user", (req, res) => {
+//   res.send("data is deleted successfully!");
+// });
 
 app.listen(7777, () => "connecting to server port number 7777");
