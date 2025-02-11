51382afb8696cd07a92d5c49d46a6f9111737eac
pramod429-git <18kppramod@gmail.com>
2025-02-11 16:59:49 +0530

    tested multiple routes

--- src/app.js
+++ src/app.js
@@ -3,29 +3,17 @@ const express = require("express");
 //calling function or creating instance of express to access its method of express
 const app = express();
 
-//to give response we have to give request handler
-
-// app.use("/user", (req, res) => {
-//   res.send("hahahahaha");
-// });
-
-app.get("/user", (req, res) => {
-  console.log(req.query); //{ id: '123', password: '321' }
-  res.send("Hi this from server");
-});
-
-// app.get("/user/:userId/:userPw", (req, res) => {
-//   //   console.log(req.params); //{ userId: '123', userPw: '321' }
-//   console.log(req.query); //{}
-//   res.send("Hi this from server");
-// });
-
-// app.post("/user", (req, res) => {
-//   res.send("data is save successfully!");
-// });
-
-// app.delete("/user", (req, res) => {
-//   res.send("data is deleted successfully!");
-// });
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("response1");
+    next();
+    // res.send("response 2");
+  },
+  (req, res, next) => {
+    console.log("response 2");
+    res.send("response 2");
+  }
+);
 
 app.listen(7777, () => "connecting to server port number 7777");
