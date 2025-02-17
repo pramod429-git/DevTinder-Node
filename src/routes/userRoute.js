1939d03139fce75adcd096cf9504bc47ad3bcce8
pramod429-git <18kppramod@gmail.com>
2025-02-17 17:46:31 +0530

    building feed api

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,6 +3,7 @@ const { authUser } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const router = express.Router();
 const USER_SAVE_DATA = "firstName lastName age gender skills";
+const User = require("../models/user");
 
 router.get("/user/review/received", authUser, async (req, res) => {
   try {
@@ -48,6 +49,46 @@ router.get("/user/connections", authUser, async (req, res) => {
     res.json({ message: "your connections", data });
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+router.get("/feed", authUser, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    //pagination
+    const page = req.query.page || 1;
+    let limit = req.query.limit || 10;
+    const skip = (page - 1) * limit;
+    limit > 50 ? 50 : limit;
+    //finding the connectionRequest db data of user fromUserId and toUserId
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+    //console.log(connectionRequest.length);
+    const hideUserConnectionRequest = new Set();
+    //finding unique value in connection request db
+    connectionRequest.forEach((req) => {
+      hideUserConnectionRequest.add(req.fromUserId._id.toString());
+      hideUserConnectionRequest.add(req.toUserId._id.toString());
+    });
+    // console.log(hideUserConnectionRequest);
+
+    //finding the user not present in the hideUserConnectionRequest from connectionRequest collection
+    const user = await User.find({
+      $and: [
+        //this condition is require already have details in the connectionRequest db
+        { _id: { $nin: Array.from(hideUserConnectionRequest) } },
+        //this condition is require not show same user details(new user)
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(USER_SAVE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.send(user);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
 });
 
